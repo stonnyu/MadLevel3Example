55380af58b373ca1b5e85e9cadf54c091e8a42fa
Tonny <mrtonnysu@gmail.com>
2020-09-25 17:37:02 +0200

    Finished reminder app

--- app/src/main/java/com/example/madlevel3example/AddReminderFragment.kt
+++ app/src/main/java/com/example/madlevel3example/AddReminderFragment.kt
@@ -6,7 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_reminder.*
+
+const val REQ_REMINDER_KEY = "req_reminder"
+const val BUNDLE_REMINDER_KEY = "bundle_reminder"
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -18,14 +25,38 @@ class AddReminderFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminders, container, false)
+        return inflater.inflate(R.layout.fragment_add_reminder, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnAddReminder.setOnClickListener {
+            onAddReminder()
+        }
+
 //        view.findViewById<Button>(R.id.button_second).setOnClickListener {
 //            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
 //        }
     }
+
+    private fun onAddReminder() {
+        val reminderText = etReminderName.text.toString()
+
+        if (reminderText.isNotBlank()) {
+            //set the data as fragmentResult, we are listening for REQ_REMINDER_KEY in RemindersFragment!
+            setFragmentResult(REQ_REMINDER_KEY, bundleOf(Pair(BUNDLE_REMINDER_KEY, reminderText)))
+
+            //"pop" the backstack, this means we destroy
+            //this fragment and go back to the RemindersFragment
+            findNavController().popBackStack()
+
+        } else {
+            Toast.makeText(
+                activity,
+                R.string.not_valid_reminder, Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 }
